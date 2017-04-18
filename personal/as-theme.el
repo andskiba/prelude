9ebd884a81c3c09df90646885030f97eb4bbd2b4
Andrzej Skiba <andrzej.skiba@nordea.com>
2017-04-18 11:55:09 +0200

    fixes to initial load

--- personal/as-theme.el
+++ personal/as-theme.el
@@ -4,7 +4,8 @@
 
 ;;; Code:
 
-(set-face-attribute 'default nil :font "Consolas-12")
+(set-face-attribute 'default nil :font "Source Code Pro-12")
+;; (set-face-attribute 'default nil :font "Consolas-12")
 
 (disable-theme 'zenburn)
 (load-theme 'deeper-blue)
